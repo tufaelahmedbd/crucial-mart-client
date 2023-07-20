2e285100f5b1193ce96e0f16d175f31646db808f
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-07-21 05:05:27 +0600

    Basic hero layout added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,75 @@
+"use client";
+import { getTransition } from "@/utils/getTransition";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
 const Hero = () => {
   return (
-    <section className="wrapper section-padding h-[calc(100vh-5rem)] ">
-      Hero
+    <section className="wrapper section-padding h-[calc(100vh-5rem)] grid grid-cols-5">
+      {/* First column */}
+      <div className=" w-full h-full ">
+        <div className="w-full h-4/6 overflow-hidden">
+          <motion.div
+            initial={{ y: "-100%" }}
+            whileInView={{ y: 0 }}
+            transition={getTransition()}
+            className="w-full h-full"
+          >
+            <Image
+              src="https://images.pexels.com/photos/11359687/pexels-photo-11359687.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Coffee Drink on Top of Book beside a Head Bust"
+              width={300}
+              height={700}
+              priority
+              className=" w-full h-full object-cover"
+            />
+          </motion.div>
+        </div>
+      </div>
+      {/* Second column */}
+      <div className=" w-full h-full"></div>
+      {/* Third column */}
+      <div className=" w-full h-full flex items-center">
+        <div className="w-full h-4/6 overflow-hidden">
+          <motion.div
+            initial={{ y: "-100%" }}
+            whileInView={{ y: 0 }}
+            transition={getTransition(0.2)}
+            className="w-full h-full"
+          >
+            <Image
+              src="https://images.pexels.com/photos/4346325/pexels-photo-4346325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Cup of Coffee on an Opened Book"
+              width={300}
+              height={700}
+              priority
+              className=" w-full h-full object-cover"
+            />
+          </motion.div>
+        </div>
+      </div>
+      {/* Fourth column */}
+      <div className=" w-full h-full "></div>
+      {/* Fifth column */}
+      <div className=" w-full h-full  flex items-end">
+        <div className="w-full h-4/6 overflow-hidden">
+          <motion.div
+            initial={{ y: "-100%" }}
+            whileInView={{ y: 0 }}
+            transition={getTransition(0.4)}
+            className="w-full h-full"
+          >
+            <Image
+              src="https://images.pexels.com/photos/2228580/pexels-photo-2228580.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Coffee & Book beside"
+              width={300}
+              height={700}
+              priority
+              className=" w-full h-full object-cover"
+            />
+          </motion.div>
+        </div>
+      </div>
     </section>
   );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { BsBag } from "react-icons/bs";
 import Link from "next/link";
 import Button from "./Button";
+import { getTransition } from "@/utils/getTransition";
 
 const Navbar = () => {
   return (
-    <header className="h-20 fixed top-0 left-0 right-0 border-b border-dark-10 bg-light/90 backdrop:blur-lg z-[100]">
+    <motion.header
+      initial={{ y: "-100%" }}
+      animate={{ y: 0 }}
+      transition={getTransition(0.6)}
+      className="h-20 fixed top-0 left-0 right-0 border-b border-dark-10 bg-light/90 backdrop:blur-lg z-[100]"
+    >
       <div className=" wrapper w-full h-full flex justify-between items-center">
         {/* nav left */}
         <nav>
@@ -37,11 +47,17 @@ const Navbar = () => {
           </ul>
         </nav>
         {/* nav right */}
-        <nav>
+        <nav className="flex items-center gap-5">
+          <Link href="/cart" className=" relative">
+            <BsBag />
+            <span className=" absolute text-xs h-4 w-4 rounded-xl bg-dark text-light flex justify-center items-center -right-2 -bottom-2">
+              10
+            </span>
+          </Link>
           <Button href="/login" placeholder="Login" color="green" />
         </nav>
       </div>
-    </header>
+    </motion.header>
   );
 };
 
